fa2f2af3322081fcd5ccc757fdd72967807b96ff
Stoodkev <quentin.correa@gmail.com>
2018-01-09 21:05:58 +0800

    Adding account value details

--- src/js/account_val.js
+++ src/js/account_val.js
@@ -64,6 +64,7 @@ function startAccountValue(isSteemit,busy,globalP,market){
 
   function createTitle(isSteemit,busy,globalP,market) {
     var real_val=document.createElement('span');
+    real_val.href="#";
     var logo=document.createElement('img');
     logo.src=chrome.extension.getURL("src/img/logo.png");
     logo.title='Real account value calculated by SteemPlus according to Bittrex price for STEEM and SBD';
@@ -75,16 +76,42 @@ function startAccountValue(isSteemit,busy,globalP,market){
     SBD_A=market.priceSBD;
     steem.api.getAccounts([account_v], function(err, result) {
       var value=0;
-      value+=STEEM_A*(parseFloat(result[0].balance.split(' ')[0])+parseFloat(result[0].savings_balance.split(' ')[0])+steem.formatter.vestToSteem(result[0].vesting_shares, globalP.totalVests, globalP.totalSteem));
-      value+=SBD_A*(parseFloat(result[0].savings_sbd_balance.split(' ')[0])+parseFloat(result["0"].sbd_balance.split(' ')[0]));
-      value=Math.round(value*100)/100;
-      value=value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      console.log(err,result);
+      const STEEM_BALANCE=STEEM_A*parseFloat(result[0].balance.split(' ')[0]);
+      const STEEM_SAVINGS=STEEM_A*+parseFloat(result[0].savings_balance.split(' ')[0]);
+      const STEEM_POWER=STEEM_A*steem.formatter.vestToSteem(result[0].vesting_shares, globalP.totalVests, globalP.totalSteem);
+      const STEEM= STEEM_BALANCE+STEEM_SAVINGS+STEEM_POWER;
+      const SBD_BALANCE=SBD_A*parseFloat(result["0"].sbd_balance.split(' ')[0]);
+      const SBD_SAVINGS=SBD_A*parseFloat(result[0].savings_sbd_balance.split(' ')[0]);
+      const SBD=SBD_BALANCE+SBD_SAVINGS;
+      const TOTAL_VALUE=SBD+STEEM;
+      value=postProcess(TOTAL_VALUE);
+
       if(isSteemit){
-        real_val.append('$ '+value);
-      wallet_elt_a.append(real_val);
+        var pop=document.createElement('a');
+        pop.innerHTML=value;
+        pop.style.cursor='pointer';
+        real_val.append(pop);
+        wallet_elt_a.append(real_val);
+
+      $('.real_value a').attr('data-toggle','popover');
+      $('.real_value a').attr('data-content','<h5>STEEM: '+postProcess(STEEM)+'</h5><hr/> Balance: '+postProcess(STEEM_BALANCE)+'<br/> SP: '+postProcess(STEEM_POWER)+'<br/> Savings: '+postProcess(STEEM_SAVINGS)+'<br/><br/> <h5>SBD: '+postProcess(SBD)+'</h5><hr/> Balance: '+postProcess(SBD_BALANCE)+'<br/> Savings: '+postProcess(SBD_SAVINGS));
+      $('.real_value a').attr('data-placement','bottom');
+      $('.real_value a').attr('title','<h5>Details</h5>');
+      $('.real_value a').attr('data-html','true');
+      $('.real_value a').click(function(){
+      $('[data-toggle="popover"]').popover();});
       if($('.real_value').length>1)
         $('.real_value')[0].remove();}
       else $('.UserWalletSummary__value')[4].title='$ '+value+' according to SteemPlus calculation using Bittrex price for SBD and STEEM.';
         url=window.location.href;
+
   });
+}
+
+function postProcess(value)
+{
+  value=Math.round(value*100)/100;
+  value=value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return '$' +value;
 }

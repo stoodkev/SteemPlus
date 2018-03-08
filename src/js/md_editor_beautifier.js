35e639d8585378e041da3196a74742a566ce7d41
steemplus <goldensuntla@hotmail.fr>
2018-03-08 18:29:27 +0800

    merge

--- src/js/md_editor_beautifier.js
+++ src/js/md_editor_beautifier.js
@@ -1,110 +1,100 @@
 var token_md_editor_beautifier=null;
 var markdownSource=null;
 var preview=null;
-var needRecreate = null;
+var waitingForPreview = null;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if(request.to==='md_editor_beautifier'&&request.order==='start'&&token_md_editor_beautifier==null&&window.location.href.includes('submit'))
     {
       token_md_editor_beautifier=request.token;
-      needRecreate = $('textarea').textLength === 0;
-
-      setupPreview();
-      $('.float-right.secondary').click(function(){
-        setupPreview();
-      });
-
-      $('textarea').bind('input propertychange', function(event){
-        if(needRecreate)
-        {
-          setupPreview();
-          needRecreate = false;
-          return;
-        }
-
-        if(event.currentTarget.value.length === 0){
-          // preview = $('.Preview');
-          preview.remove();
-          needRecreate = true;
-        }
-      });
+      startMDEditorPreview(); 
     }
     if(request.to==='md_editor_beautifier'&&request.order==='click'&&window.location.href.includes('submit'))
     {
       token_md_editor_beautifier=request.token;
-      needRecreate = $('textarea').textLength === 0;
-
-      setupPreview();
-      $('.float-right.secondary').click(function(){
-        setupPreview();
-      });
-
-      $('textarea').bind('input propertychange', function(event){
-        if(needRecreate)
-        {
-          setupPreview();
-          needRecreate = false;
-          return;
-        }
-
-        if(event.currentTarget.value.length === 0){
-          // preview = $('.Preview');
-          preview.remove();
-          needRecreate = true;
-        }
-      });
+      startMDEditorPreview();
     }
 });
 
-
-function setupPreview(){
-  console.log('test');
-  if($('.RichTextEditor__root___33zoV').length > 0)
+function startMDEditorPreview()
+{
+  if($('textarea').length > 0 && $('textarea')[0].textLength === 0)
   {
-    // Using markdown editor so put editor in the middle
-    $('.myrow').addClass('column');
-    $('.myrow').addClass('RichTextEditor__root___33zoV');
-    $('.column.small-12').removeClass('myrow');
+    waitingForPreview = true;
   }
   else
+    waitingForPreview = false;
+  console.log($('textarea').textLength == 0);
+
+  setupPreview();
+  $('.float-right.secondary').click(function(){
+    bindTextArea();
+  });
+  bindTextArea();
+  
+}
+
+function bindTextArea()
+{
+  if($('textarea').length === 0)
   {
-    $('.MarkdownViewer').bind('DOMSubtreeModified', function(event) {
-      $('.MarkdownViewer > div')[1].innerHTML = $('.MarkdownViewer > div')[0].outerHTML;
-    });
-
-    if($('.Preview').length === 0)
-    {
-        setTimeout(function(){
-          setupPreview();
-        }, 200);
-        return;
-    }
-
-    // Put editor next to preview
-    markdownSource=$('.Preview');
-    preview = markdownSource.clone();
-    markdownSource.hide();
-
-    if($('.myrow').length > 0)
-    {
-      preview.appendTo($('.myrow'));
+    setTimeout(function(){
+      bindTextArea();
+    }, 1000);
+  }
+  
+  $('textarea').bind('input propertychange', function(event){
+    if(event.currentTarget.value.length === 0){
+      preview.remove();
+      waitingForPreview = true;
+      console.log('change property 0');
+      setupPreview();
     }
     else
     {
-      preview.appendTo($('.column'));
-      $('.column').removeClass('small-12');
-      $('.column').addClass('row');
-      $('.column').addClass('myrow');
-      $('.column').removeClass('column');
-
-      $('.ReplyEditor').removeClass('row');
+      console.log('added letter');
+      waitingForPreview = false;
     }
+  });
+} 
+
+function setupPreview(){
+  $('.MarkdownViewer').bind('DOMSubtreeModified', function(event) {
+    $('.MarkdownViewer > div')[1].innerHTML = $('.MarkdownViewer > div')[0].outerHTML;
+  });
+
+  if(waitingForPreview || $('.Preview').length === 0)
+  {
+      console.log('No preview (waiting==' + waitingForPreview + ' &&length==' + $('.Preview').length);
+      setTimeout(function(){
+        setupPreview();
+      }, 200);
+      return;
   }
 
-  
+  console.log($('.Preview'));
 
+  // Put editor next to preview
+  markdownSource=$('.Preview');
+  preview = markdownSource.clone();
+  preview.id = 'mypreview';
+  markdownSource.hide();
 
+  if($('.myrow').length > 0)
+  {
+    console.log('ici');
+    preview.appendTo($('.myrow'));
+  }
+  else
+  {
+    console.log('la');
+    preview.appendTo($('.column'));
+    $('.column').removeClass('small-12');
+    $('.column').addClass('row');
+    $('.column').addClass('myrow');
+    $('.column').removeClass('column');
 
-
+    $('.ReplyEditor').removeClass('row');
+  }
 }
 

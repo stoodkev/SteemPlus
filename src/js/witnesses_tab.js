c60b470a95d37b8707378030299fc4f2d4dbcd72
steemplus <goldensuntla@hotmail.fr>
2018-04-10 17:15:09 +0800

    Tab Witnesses updated

--- src/js/witnesses_tab.js
+++ src/js/witnesses_tab.js
@@ -56,8 +56,8 @@ function createTabWitnesses(witnessesTab)
               <h1 class="articles__h1 addWitness" style="margin-bottom:20px; margin-top:20px;">\
                 Add a new witness\
               </h1>\
-              <div class="input-group" style="margin-bottom: 5px;">\
-                <input type="text" placeholder="Witness\' name" name="witnessName" value="" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" autofocus="">\
+              <div class="input-group inputAddNewWitness" style="margin-bottom: 5px;">\
+                <input id="addWitnessName" type="text" placeholder="Witness\' name" name="witnessName" value="" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" autofocus="">\
                 <span class="input-group-label" style="padding-left: 0px; padding-right: 0px;">\
                   <button id="addWitnessButton" name="asset" placeholder="Asset">\
                     Add\
@@ -72,16 +72,57 @@ function createTabWitnesses(witnessesTab)
 
   $('#addWitnessButton').click(function()
   {
-    console.log(this.value);
-    steem.api.getAccounts([this.value], function(err, result) {
-      if(err) console.log(err);
+    var newWitnessName = $('#addWitnessName')[0].value;
+    steem.api.getAccounts([newWitnessName], function(err, result) {
+      
+      toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-bottom-center",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+      };
+      var titleToastr = "SteemPlus - Vote for witness";
+
+      if(err) 
+      {
+        console.log(err);
+        toastr.error('Unknown error, Please try again later', titleToastr);
+      }
       else
       {
-        console.log('Add as witness ' + this.value);
+        if(result.length > 0)
+        {
+          var win = window.open('https://v2.steemconnect.com/sign/account-witness-vote?witness=' + newWitnessName + '&approve=1', '_blank');
+          if (win) {
+              win.focus();
+          } else {
+              alert('Please allow popups for this website');
+          }
+        }
+        else
+        {
+          toastr.error('Unknown user. Please check the username.', titleToastr);
+        }
       }
     });
   });
 
+  addListWitness(usernameTabWitnesses, isMyPageWitnesses);
+}
+
+function addListWitness(usernameTabWitnesses, isMyPageWitnesses)
+{
   steem.api.getAccounts([usernameTabWitnesses], function(err, result) {
     if(err) console.log(err);
     else
@@ -101,22 +142,33 @@ function createTabWitnesses(witnessesTab)
         if(a > b) return 1;
         return 0;
       });
-      listWitnesses.forEach(function(witnessItem){
-        $(rowWitness).append('<div class="col-4 witness-cells"><h5 class="witness-items"><a href="https://www.steemit.com/@' + witnessItem + '">@' + witnessItem + '</a></h5></div>');
-        $(rowWitness).append($('<div class="col-4"></div>'));
+      listWitnesses.forEach(function(witnessItem, index){
+        var classOddEven = '';
+        if(index%2===0) classOddEven = 'evenLine';
+
+        $(rowWitness).append('<div class="col-4 witness-cells ' + classOddEven + '"><h5 class="witness-items"><a href="https://www.steemit.com/@' + witnessItem + '">@' + witnessItem + '</a></h5></div>');
+        $(rowWitness).append($('<div class="col-4 ' + classOddEven + '"></div>'));
         if(isMyPageWitnesses)
         {
-          var buttonRemoveWitness = $('<div class="col-4 witness-cells"><label class="button slim hollow primary removeWitnessesLink witness-items" id="' + witnessItem + '">Remove from my witnesses</label></div>');
+          var divButtonRemoveWitness = $('<div class="col-4 witness-cells ' + classOddEven + '"></div>');
+          var buttonRemoveWitness = $('<label class="button slim hollow primary removeWitnessesLink witness-items" id="' + witnessItem + '">Remove from my witnesses</label>');
           
           $(buttonRemoveWitness).click(function(){
-            console.log('Unfollow' + this.id);
+            var win = window.open('https://v2.steemconnect.com/sign/account-witness-vote?witness=' + this.id + '&approve=0', '_blank');
+            if (win) {
+                win.focus();
+            } else {
+                alert('Please allow popups for this website');
+            }
           });
-          $(rowWitness).append(buttonRemoveWitness);
+
+          $(divButtonRemoveWitness).append(buttonRemoveWitness);
+          $(rowWitness).append(divButtonRemoveWitness);
           $(rowWitness).append(witnessSeparator); 
         }
         else
         {
-          $(rowWitness).append($('<div class="col-4 witness-cells"></div>'));
+          $(rowWitness).append($('<div class="col-4 witness-cells ' + classOddEven + '"></div>'));
           $(rowWitness).append(witnessSeparator);
         }
         

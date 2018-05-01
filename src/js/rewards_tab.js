27e4a1512793ac667cd7f39028b21a624e0b5071
steemplus <goldensuntla@hotmail.fr>
2018-05-01 22:25:50 +0800

    Add ajax request to get rewards informations

--- src/js/rewards_tab.js
+++ src/js/rewards_tab.js
@@ -1,6 +1,9 @@
 var token_rewards_tab=null;
 var totalVestsRewardsTab=null
 var totalSteemRewardsTab=null;
+var downloadingDataRewardTab=false;
+
+var rewardsListLocal=null;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
@@ -61,6 +64,14 @@ function createRewardsTab(rewardsTab)
 	            <input type="radio" id="rewards-type-both" name="rewards-type" class="rewards-type" value="benefactor" />\
 	            <label for="rewards-type-both" class="rewards-type">Benefactor</label>\
 	          </div>\
+	          <div class="row">\
+	          	<div class="columns small-10 medium-12 medium-expand">\
+	          		<ul class="WalletSubMenu menu RewardsTabSubMenu">\
+	          			<li><a name="pending" class="subtypeItem active">Pending</a></li>\
+	          			<li><a name="paid" class="subtypeItem">Paid</a></li>\
+	          		</ul>\
+	          	</div>\
+	          </div>\
 	        </div>\
 	        <center class="RewardsTabLoading">\
 	           <div class="LoadingIndicator circle">\
@@ -76,9 +87,82 @@ function createRewardsTab(rewardsTab)
      </div>\
   </div>');
 
+	rewardsTab.find('.rewards-type').on('change', function() {
+    // Change display
+    var typeReward = (rewardsTab.find('.rewards-type:checked')[0].value);
+    displayRewards(rewardsTab, typeReward,'pending',window.SteemPlus.Utils.getPageAccountName());
+  });
 
-
-  $('.Rewards').show();
-  $('.RewardsTabLoading').hide();
+  rewardsTab.find('.RewardsTabSubMenu > ul > li > a').click(function(){
+		var typeReward = (rewardsTab.find('.rewards-type:checked')[0].value);
+		var subTypeReward = $(this).attr('name');
+		console.log($(this));
+		$('.subtypeItem').removeClass('active');
+		$(this).addClass('active');
+		displayRewards(rewardsTab, typeReward,'pending',window.SteemPlus.Utils.getPageAccountName());
+  });
+  // Display mentions in post
+  displayRewards(rewardsTab,'author', 'pending', window.SteemPlus.Utils.getPageAccountName());
 }
 
+// Display rewards depending on type and sub type
+// @parameter reward : jquery object, tab
+// @parameter type : represent the selected element (Author, Curation or Benefactor)
+// @parameter subtype : represent the subcategory of the selected element (pending or paid)
+// @parameter usernamePageReward : selected user
+function displayRewards(rewardsTab, type, subtype, usernamePageReward)
+{
+	if(rewardsListLocal===null&&!downloadingDataRewardTab)
+	{
+		// No data and not downloading
+		$.ajax({
+      type: "GET",
+      beforeSend: function(xhttp) {
+        downloadingDataRewardTab=true;
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.setRequestHeader("X-Parse-Application-Id", chrome.runtime.id);
+      //  console.log(xhttp);
+      },
+
+        // url: 'http://steemplus-api.herokuapp.com/api/get-mentions/'+ usernamePageMentions,
+        url: 'https://api.myjson.com/bins/uyr5z',
+      success: function(result) {
+        rewardsListLocal = result;
+        createRowsRewardsTab(rewardsTab, type, subtype);
+        downloadingDataRewardTab = false;
+      },
+      error: function(msg) {
+        downloadingDataRewardTab = false;
+        if($('.error-mentions-label').length===0){
+          var errorLabel = document.createElement('h2');
+          $(errorLabel).addClass('articles__h1');
+          $(errorLabel).addClass('error-mentions-label');
+          $(errorLabel).append('Looks like we are having trouble retrieving information from steemSQL. Please try again later.');
+          $('.MentionsTabLoading').hide();
+          $('.articles').prepend(errorLabel);
+        }
+      }
+    });
+	}
+	else
+	{
+		if(rewardsListLocal!==null)
+		{
+			// Use local data
+			createRowsRewardsTab(rewardsTab, type, subtype)
+		}
+		else
+		{
+			setTimeout(function(){
+        displayRewards(rewardsTab, type, subtype, usernamePageReward);
+      },250);
+		}
+	}
+}
+
+function createRowsRewardsTab(rewardsTab, type, subtype)
+{
+	console.log(rewardsListLocal);
+	$('.Rewards').show();
+  $('.RewardsTabLoading').hide();
+}

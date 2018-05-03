97c85f5841eabd9bb5b572a1ae5ab200e343a273
steemplus <goldensuntla@hotmail.fr>
2018-05-03 08:59:56 +0800

    Displayed rewards information

--- src/js/rewards_tab.js
+++ src/js/rewards_tab.js
@@ -25,14 +25,17 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	}
 });
 
+// Start Tab Reward if it can
 function startTabReward()
 {
+	// Wait for the nav bar to be ready
 	if($('.UserProfile__top-nav').length === 0)
 		setTimeout(function(){
 			startTabReward();
 		}, 250);
 	else
 	{
+		// Create the new tab
 		console.log($('.UserProfile__top-nav > div > div > ul > li'));
 		$('.UserProfile__top-nav > div > div > ul > li').eq(3).hide();
 		window.SteemPlus.Tabs.createTab({
@@ -41,11 +44,16 @@ function startTabReward()
 			enabled: true,
 			createTab: createRewardsTab
 		});
+
+		// Display the tab if #rewards in url
 		if(window.location.href.includes('#rewards'))
 			window.SteemPlus.Tabs.showTab('rewards');
 	}
 }
 
+
+// Function used to create the tab content
+// @parameter rewardTab : graphical element tab
 function createRewardsTab(rewardsTab)
 {
 	rewardsTab.html('<div class="row">\
@@ -65,20 +73,18 @@ function createRewardsTab(rewardsTab)
 						<label for="rewards-type-both" class="rewards-type">Benefactor</label>\
 					</div>\
 					<div class="row">\
-						<div class="columns small-10 medium-12 medium-expand">\
+						<div class="columns small-10 medium-12 medium-expand" style="padding:0px;">\
 							<ul class="WalletSubMenu menu RewardsTabSubMenu">\
 								<li><a name="pending" class="subtypeItem active">Pending</a></li>\
 								<li><a name="paid" class="subtypeItem">Paid</a></li>\
 							</ul>\
 						</div>\
-						<div>\
-							<div class="bootstrap-wrapper">\
-			          <div class="container container-rewards">\
-			          	<div class="row"></div>\
-			          </div>\
-			        </div>\
-						</div>\
 					</div>\
+					<div class="bootstrap-wrapper">\
+	          <div class="container container-rewards">\
+	          	<div class="row"></div>\
+	          </div>\
+	        </div>\
 				</div>\
 				<center class="RewardsTabLoading">\
 					<div class="LoadingIndicator circle">\
@@ -94,18 +100,20 @@ function createRewardsTab(rewardsTab)
 		</div>\
 	</div>');
 
+	// On change type
 	rewardsTab.find('.rewards-type').on('change', function() {
     // Change display
     var typeReward = (rewardsTab.find('.rewards-type:checked')[0].value);
     displayRewards(rewardsTab, typeReward,'pending',window.SteemPlus.Utils.getPageAccountName());
 	});
 
+	// Change subtype
 	rewardsTab.find('.subtypeItem').click(function(){
 		var typeReward = (rewardsTab.find('.rewards-type:checked')[0].value);
 		var subTypeReward = $(this).attr('name');
 		$('.subtypeItem').removeClass('active');
 		$(this).addClass('active');
-		displayRewards(rewardsTab, typeReward,'pending',window.SteemPlus.Utils.getPageAccountName());
+		displayRewards(rewardsTab, typeReward ,subTypeReward, window.SteemPlus.Utils.getPageAccountName());
 	});
 
   // Display mentions in post
@@ -129,26 +137,26 @@ function displayRewards(rewardsTab, type, subtype, usernamePageReward)
 				xhttp.setRequestHeader("Content-type", "application/json");
 				xhttp.setRequestHeader("X-Parse-Application-Id", chrome.runtime.id);
       //  console.log(xhttp);
-  },
+  		},
 
-        // url: 'http://steemplus-api.herokuapp.com/api/get-mentions/'+ usernamePageMentions,
-        url: 'https://api.myjson.com/bins/uyr5z',
-        success: function(result) {
-        	rewardsListLocal = result;
-        	createRowsRewardsTab(rewardsTab, type, subtype);
-        	downloadingDataRewardTab = false;
-        },
-        error: function(msg) {
-        	downloadingDataRewardTab = false;
-        	if($('.error-mentions-label').length===0){
-        		var errorLabel = document.createElement('h2');
-        		$(errorLabel).addClass('articles__h1');
-        		$(errorLabel).addClass('error-mentions-label');
-        		$(errorLabel).append('Looks like we are having trouble retrieving information from steemSQL. Please try again later.');
-        		$('.MentionsTabLoading').hide();
-        		$('.articles').prepend(errorLabel);
-        	}
-        }
+      // url: 'http://steemplus-api.herokuapp.com/api/get-mentions/'+ usernamePageMentions,
+      url: 'https://api.myjson.com/bins/142hsa',
+      success: function(result) {
+      	rewardsListLocal = result;
+      	createRowsRewardsTab(rewardsTab, type, subtype);
+      	downloadingDataRewardTab = false;
+      },
+      error: function(msg) {
+      	downloadingDataRewardTab = false;
+      	if($('.error-mentions-label').length===0){
+      		var errorLabel = document.createElement('h2');
+      		$(errorLabel).addClass('articles__h1');
+      		$(errorLabel).addClass('error-mentions-label');
+      		$(errorLabel).append('Looks like we are having trouble retrieving information from steemSQL. Please try again later.');
+      		$('.RewardsTabLoading').hide();
+      		$('.articles').prepend(errorLabel);
+      	}
+      }
     });
 	}
 	else
@@ -167,35 +175,36 @@ function displayRewards(rewardsTab, type, subtype, usernamePageReward)
 	}
 }
 
+// Create all the row depending on type and subtype
+// @parameter rewardTab : tab's graphical element
+// @parameter type : type displayed (Author, Curation, Benefactor)
+// @parameter subtype : subtype displayed (Pending or Paid)
 function createRowsRewardsTab(rewardsTab, type, subtype)
 {
-	$('.Rewards').show();
-	$('.RewardsTabLoading').hide();
+	$('.Rewards').hide();
+	$('.RewardsTabLoading').show();
 	$('.container-rewards').find('.row').empty();
 	var indexDisplayReward = 0;
-	var style = 'background-color:#fcfcfc;';
+	var style = 'background-color:#eeeeee;';
 	rewardsListLocal.forEach(function(item){
 		if(item.type===subtype + '_' + type)
 		{
-			var rewardText = null;
+			var rewardText = [];
 			if(item.reward===-1)
 			{
-				var rewardSBDText = (item.sbd_payout===0 ? null : item.sbd_payout + ' SBD');
-				var rewardSPText = null;
-				if(item.vests_payout===0)
-				{
-					rewardSPText = steem.formatter.vestsToSteem(parseFloat(item.rewards), totalVestsRewardsTab, totalSteemRewardsTab) + ' SP';
-				}
-				var rewardSteemText = (item.steem_payout===0 ? null : item.steem_payout + ' STEEM');
-			
-				rewardText = (rewardSBDText===null ? '' : rewardSBDText + ' ') + (rewardSPText===null ? '' : rewardSPText + ' ') + (rewardSteemText===null ? '' : rewardSteemText + ' ');
+				if(item.sbd_payout>0) rewardText.push(item.sbd_payout.toFixed(3) + ' SBD');
+				if(item.vests_payout>0) rewardText.push(steem.formatter.vestToSteem(parseFloat(item.vests_payout), totalVestsRewardsTab, totalSteemRewardsTab).toFixed(3) + ' SP');
+				if(item.steem_payout>0) rewardText.push(item.steem_payout.toFixed(3) + ' STEEM');
 			}
 			else
 			{
-				rewardText = steem.formatter.vestsToSteem(parseFloat(item.rewards), totalVestsRewardsTab, totalSteemRewardsTab) + ' SP';
+				rewardText.push(steem.formatter.vestToSteem(parseFloat(item.reward), totalVestsRewardsTab, totalSteemRewardsTab).toFixed(3) + ' SP');
 			}
-			$('.container-rewards').find('.row').append('<span style="' + (indexDisplayReward%2===0 ? style : '') + '" class="col-2" title="' + new Date(item.timestamp) + '">' + moment(new Date(item.timestamp)).fromNow() + '</span> <span style="' + (indexDisplayReward%2===0 ? style : '') + '" class="col-3">' + rewardText + '</span><span style="' + (indexDisplayReward%2===0 ? style : '') + '" class="col-6">' + item.permlink + '</span>');
+			$('.container-rewards').find('.row').append('<span style="' + (indexDisplayReward%2===0 ? style : '') + '" class="col-2" title="' + new Date(item.timestamp) + '">' + moment(new Date(item.timestamp)).fromNow() + '</span> <span style="' + (indexDisplayReward%2===0 ? style : '') + '" class="col-4">' + rewardText.join(', ') + '</span><span style="' + (indexDisplayReward%2===0 ? style : '') + '" class="col-6">' + item.permlink + '</span>');
 			indexDisplayReward++;
 		}
 	});
+
+	$('.Rewards').show();
+	$('.RewardsTabLoading').hide();
 }

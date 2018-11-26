54cd1444962d919e0fc7c7029f4670fc7e4cc185
steemplus <goldensuntla@hotmail.fr>
2018-11-26 15:52:23 +0800

    Added base for tabs

--- src/js/premium_features.js
+++ src/js/premium_features.js
@@ -1,8 +1,25 @@
 let token_premium_features = null;
 
+const features = [
+  {
+    "name": "Remove Ads",
+    "description": "This feature allows you to remove ads on the page",
+    "price": "10 SPP/month"
+  },
+  {
+    "name": "Cancel beneficiaries",
+    "description": "This feature allows you to cancel the 5% beneficiaries if you use the beneficiaries feature.",
+    "price": "10 SPP/month"
+  },
+  {
+    "name": "Statistics",
+    "description": "Add a pixel to get statistics about post view",
+    "price": "10 SPP/month"
+  }
+]
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.to === 'premium_features' && request.order === 'start' && token_premium_features == null) {
-      console.log('ici');
         token_premium_features = request.token;
         startPremiumFeatures()
     } 
@@ -13,42 +30,115 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 function startPremiumFeatures() {
     if (regexBlogSteemit.test(window.location.href)) {
-        window.SteemPlus.Tabs.createTab({
-            id: 'premium_features',
-            title: 'Premium Features',
-            enabled: true,
-            createTab: createTabPremiumFeature,
-            newDropdown: true,
-            nameDropdown: "premium_features",
-            labelDropdown: "Premium Features"
-        });
-        if (window.location.href.includes('#premium_features'))
-            window.SteemPlus.Tabs.showTab('premium_features');
+      window.SteemPlus.Tabs.createTab({
+        id: 'my_account_premium',
+        title: 'My Account',
+        enabled: true,
+        createTab: createTabMyAccountPremium,
+        newDropdown: true,
+        nameDropdown: "premium_features",
+        labelDropdown: "Premium Features"
+      });
+
+      window.SteemPlus.Tabs.createTab({
+        id: 'feature_list',
+        title: 'Feature List',
+        enabled: true,
+        createTab: createTabPremiumFeatureList,
+        newDropdown: true,
+        nameDropdown: "premium_features",
+        labelDropdown: "Premium Features"
+      });
+
+      if (window.location.href.includes('#my_account_premium'))
+          window.SteemPlus.Tabs.showTab('my_account_premium');
+      else if (window.location.href.includes('#feature_list'))
+          window.SteemPlus.Tabs.showTab('feature_list');
     }
 
 }
 
-function createTabPremiumFeature(premiumFeatures) {
-    premiumFeatures.html('\
+function createTabMyAccountPremium(myAccountPremium) {
+    myAccountPremium.html('\
     <div class="feed-layout container">\
       <div class="row">\
         <div class="UserProfile__tab_content UserProfile__tab_content_smi UserProfile__tab_content_PremiumFeatures column layout-list">\
           <article class="articles">\
             <div class="premiumFeaturesTab">\
               <h1 class="articles__h1" style="margin-bottom:20px">\
-                Witnesses\
+                My Account\
               </h1>\
               <hr class="articles__hr"/>\
-                <div class="LoadingIndicator circle">\
+                <div class="LoadingIndicator loading-account circle">\
                   <div></div>\
                 </div>\
               </center>\
-              <div class="premium-feature-content"></div>\
+              <div class="my-account-premium-content"></div>\
               <br>\
             </div>\
           </article>\
         </div>\
       </div>\
     </div>');
+
+    $('.my-account-premium-content').append(`
+        <div class="feature row">
+          <div class="column small-12 medium-8 description-feature">
+            My SPP
+            <div class="FormattedHTMLMessage secondary">
+              SPP allow you to receive a share of @steem-plus vote and are redeemable for premium features.
+            </div>
+          </div>
+          <div class="column small-12 medium-4 payment-feature">
+            <p>${10} SPP</p>
+          </div>
+        </div>`
+      );
+
+    $('.loading-account').hide();
+
+}
+
+function createTabPremiumFeatureList(premiumFeatureList) {
+    premiumFeatureList.html('\
+    <div class="feed-layout container">\
+      <div class="row">\
+        <div class="UserProfile__tab_content UserProfile__tab_content_smi UserProfile__tab_content_PremiumFeatures column layout-list">\
+          <article class="articles">\
+            <div class="premiumFeaturesTab">\
+              <h1 class="articles__h1" style="margin-bottom:20px">\
+                Feature List\
+              </h1>\
+              <hr class="articles__hr"/>\
+                <div class="LoadingIndicator loading-feature-list circle">\
+                  <div></div>\
+                </div>\
+              </center>\
+              <div class="premium-feature-list-content"></div>\
+              <br>\
+            </div>\
+          </article>\
+        </div>\
+      </div>\
+    </div>');
+
+    features.forEach((feature, index) => {
+      $('.premium-feature-list-content').append(`
+        <div class="feature row ${index % 2 === 0 ? 'feature-even' : ''}">
+          <div class="column small-12 medium-8 description-feature">
+            ${feature.name}
+            <div class="FormattedHTMLMessage secondary">
+              ${feature.description}
+            </div>
+          </div>
+          <div class="column small-12 medium-4 payment-feature">
+            <p>${feature.price}</p>
+            <button class="button-steemit-subscribe">Subscribe</button>
+          </div>
+        </div>`
+      );
+    });
+
+    $('.loading-feature-list').hide();
 
 }

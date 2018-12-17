6303c0d019bd697d9a7a0b4d5c3b6f1717218608
steemplus <goldensuntla@hotmail.fr>
2018-12-17 15:46:48 +0800

    Added comments to premium-features

--- src/js/premium_features.js
+++ src/js/premium_features.js
@@ -1,6 +1,6 @@
 let token_premium_features = null;
 let activePremiumFeaturesSubscriptionsUser = null;
-const accountName = 'lecaillon';
+const accountName = 'steem-plus';
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.to === 'premium_features' && request.order === 'start' && token_premium_features == null) {
@@ -14,6 +14,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
 });
 
+
+// Function used to verify if the feature has to start or not
 function startPremiumFeatures() {
     if (regexBlogSteemit.test(window.location.href)) {
       window.SteemPlus.Tabs.createTab({
@@ -32,11 +34,18 @@ function startPremiumFeatures() {
 
 }
 
+// Function used to create the tab content
+// @param : premiumFeatureList : HTML element representing the tab
 function createTabPremiumFeatureList(premiumFeatureList) {
+
+  // Calculate the total for all the subscriptions
+  // This will help user to know how much they spend per month
   let totalAmountSubscription = 0;
   activePremiumFeaturesSubscriptionsUser.map(function(sub){
     totalAmountSubscription += sub.premiumFeature.price;
   });
+
+  // Create tab content
   premiumFeatureList.html('\
   <div class="feed-layout container">\
     <div class="row">\
@@ -62,6 +71,7 @@ function createTabPremiumFeatureList(premiumFeatureList) {
     </div>\
   </div>');
 
+  // API call to download the list of all the premium features
   $.ajax({
     type: "GET",
     beforeSend: function(xhttp) {
@@ -71,8 +81,14 @@ function createTabPremiumFeatureList(premiumFeatureList) {
     url: 'https://api.steemplus.app/premium-feature-list',
     success: function(features) {
         features.forEach((feature, index) => {
+
+            // Check is a feature is active for user's account
             const isActive = findFeature(feature.name) !== undefined;
             const activeFeature = findFeature(feature.name);
+
+            // Create line in tab.
+            // If feature is active, add unsubscribe button,
+            // If not create subscribe button
             $('.premium-feature-list-content').append(`
               <div class="feature row ${index % 2 === 0 ? 'feature-even' : ''}">
                 <div class="column small-12 medium-8 description-feature">
@@ -89,24 +105,32 @@ function createTabPremiumFeatureList(premiumFeatureList) {
               </div>`
             );
         });
+
+      // When page is ready, remove loading circle
       $('.loading-feature-list').hide();
 
+      // Create event listener for subscribe buttons
       $('.button-steemit-subscribe').click(event => {
         subscribeFeature(event.target.name);
       });
 
+      // Create event listener for unsubscribe buttons
       $('.button-steemit-unsubscribe').click(event => {
         unsubscribeFeature(event.target.name);
       });
     },
     error: function(msg) {
+        // If error while downloading data, display error message
         $('.premium-feature-list-content').append(`<h4>Seems that Steemplus-API is having trouble. Please try again later. Sorry for the inconvenience.</h4>
           <h4>SteemPlus Team</h4>`);
     }
   });
 }
 
+// Function used to create cancel message
+// @param feature : feature we need to get the message for
 function getCancelMessage(feature){
+  // If feature is not active, no cancel message
   if(feature !== undefined && feature.isCanceled)
     return `<div class="FormattedHTMLMessage secondary cancel-message">
               Your subscription will be canceled on ${moment(new Date(feature.lastPayment)).add(1, 'month')}. If you want to still use it, subscribe again (you won't be charged again). 
@@ -114,7 +138,11 @@ function getCancelMessage(feature){
   else return '';
 }
 
+// Function used to create the subscription panel for a given feature
+// @param isActive : status of the given feature
+// @param feature : given feature
 function getSubscriptionPanel(isActive, feature){
+  // Only If the feature is active create the panel
   if(isActive)
     return `<div class="FormattedHTMLMessage secondary" title="${new Date(feature.subscriptionDate)}">
               You subscribe to ${feature.premiumFeature.name} ${moment(new Date(feature.subscriptionDate)).fromNow()}.
@@ -125,22 +153,31 @@ function getSubscriptionPanel(isActive, feature){
   else return '';
 }
 
+// Function used to find a feature in the feature list
+// @param name : name of the feature
 function findFeature(name) {
   return activePremiumFeaturesSubscriptionsUser.find(function(sub) {
     return name === sub.premiumFeature.name;
   })
 }
 
+// function used to subscribe to a feature
+// @param nameFeature : name of the feature
 function subscribeFeature(nameFeature){
   const memo = `Premium Feature : Redeem SPP for [${nameFeature}] id:${generateRequestID(12)}`;
   sendTransfer(memo);
 }
 
+// function used to unsubscribe to a feature
+// @param nameFeature : name of the feature
 function unsubscribeFeature(nameFeature){
   const memo = `Premium Feature : Cancel subscription for [${nameFeature}] id:${generateRequestID(12)}`;
   sendTransfer(memo);  
 }
 
+
+// Function used to send the transfer to SteemPlus
+// @param memo : memo discribing the purpose of the transfer
 function sendTransfer(memo){
   const url = 'https://steemconnect.com/sign/transfer?to=' + encodeURIComponent(accountName) + '&amount=0.001%20SBD&memo=' + encodeURIComponent(memo);
   var win = window.open(url, '_blank');
@@ -153,6 +190,7 @@ function sendTransfer(memo){
   }
 }
 
+// Function used to generate a random requestID
 function generateRequestID(length){
   let requestID = "";
     var possible = "0123456789";
